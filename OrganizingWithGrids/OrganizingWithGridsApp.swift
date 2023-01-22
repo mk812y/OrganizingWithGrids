dec1d49d46940b0d775ef75117ae769da883ddca
mk812y <mk812y@gmail.com>
2023-01-22 17:55:55 +0300

    Initial Commit

---
+++ OrganizingWithGrids/OrganizingWithGridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrganizingWithGridsApp.swift
+//  OrganizingWithGrids
+//
+//  Created by Михаил Куприянов on 22.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct OrganizingWithGridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
